6ae26ac20da6938c2f374f8f35a5daaeec9ba2dd
Bobby Kawade <56881505+bobbykawade@users.noreply.github.com>
2021-03-10 16:25:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from PIL import Image
 
 
 
-st.markdown("<h1 style='text-align: center; color: #157DDA;'><strong><u>Predict Blood Transfusion with Logistic Regression</u></strong></h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #FFBF00;'><strong><u>Predict Blood Transfusion with Logistic Regression</u></strong></h1>", unsafe_allow_html=True)
 
 image = Image.open('blood.jpg')
 st.image(image,width=700)
 
-st.sidebar.markdown("<h1 style='text-align: center; color:#157DDA ;'><strong><u>Specify Input Parameters</u></strong></h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<h1 style='text-align: center; color:#FFBF00 ;'><strong><u>Specify Input Parameters</u></strong></h1>", unsafe_allow_html=True)
     
 st.markdown("Forecasting blood supply is a serious and recurrent problem for blood collection managers: in January 2019, Nationwide, the Red Cross saw 27,000 fewer blood donations over the holidays than they see at other times of the year. Machine learning can be used to learn the patterns in the data to help to predict future blood donations and therefore save more lives.")
 st.markdown("Understanding the Parameters -")
